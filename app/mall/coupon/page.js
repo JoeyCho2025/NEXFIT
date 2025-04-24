8a8417c14ae3480308cbebc366ae0d6c38f8ad60
Cadecc <fgh456hgf@gmail.com>
2025-04-24 09:56:40 +0800

    V10-1
    會員中心商城的頁面

---
+++ app/mall/coupon/page.js
@@ -0,0 +1,27 @@
+"use client";
+
+export default function couponPage() {
+  return (
+    <div className=" p-10 w-4/5 h-full mt-10 ml-10 mr-15 border border-borderColor rounded-lg ">
+      <div>這</div>
+      <div>邊</div>
+      <div>刻</div>
+      <div>你</div>
+      <div>頁</div>
+      <div>面</div>
+      <div>的</div>
+      <div>內</div>
+      <div>容</div>
+      <div>，</div>
+      <div>外</div>
+      <div>框</div>
+      <div>會</div>
+      <div>根</div>
+      <div>據</div>
+      <div>內</div>
+      <div>容</div>
+      <div>長</div>
+      <div>大</div>
+    </div>
+  );
+}
